c94821b44e3a9a1282847f192810ed864cfcfd9c
Michal Staniecko <michal.staniecko@gmail.com>
2021-05-08 20:06:45 +0200

    stopka

--- wp-content/themes/sardynkibiznesu-2.0/footer.php
+++ wp-content/themes/sardynkibiznesu-2.0/footer.php
@@ -1,3 +1,30 @@
+<footer class="footer">
+  <div class="footer__main">
+    <div class=" container-fluid container-fluid-stop">
+      <div class="row">
+        <div class="col">
+          <p>
+            <strong>SardynkiBiznesu.pl</strong> to blog i podcast, na których znajdziesz porady dotyczące prowadzenia małej firmy. Dzięki
+            nim oszczędzisz dużo czasu, pieniędzy i usprawnisz funkcjonowanie swojego biznesu.
+          </p>
+        </div>
+        <div class="col">
+          <ul>
+            <li><a href="#">O NAS</a></li>
+            <li><a href="#">FACEBOOK</a></li>
+            <li><a href="#">YOUTUBE</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer__bottom">
+    <div class=" container-fluid container-fluid-stop" itemscope="itemscope" itemtype="https://schema.org/WPFooter">
+      Kontakt - Polityka prywatności - Regulamin strony / SardynkiBiznesu.pl © 2020
+    </div>
+  </div>
+</footer>
+
 </div>
 
 <?php wp_footer(); ?>
