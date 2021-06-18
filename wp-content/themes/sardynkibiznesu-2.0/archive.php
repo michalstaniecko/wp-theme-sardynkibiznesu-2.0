f1169bda52660acef2e1d19b374f864d8dff9887
Michal Staniecko <michal.staniecko@gmail.com>
2021-06-18 23:29:54 +0200

    breadcrumbs

--- wp-content/themes/sardynkibiznesu-2.0/archive.php
+++ wp-content/themes/sardynkibiznesu-2.0/archive.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
   <div class="page__wrapper">
+
+    <div class="breadcrumbs">
+      <div class="container-fluid container-fluid-stop">
+        <?php
+        if (function_exists('yoast_breadcrumb')) {
+          yoast_breadcrumb('<div class="breadcrumbs__wrapper">', '</div>');
+        }
+        ?>
+      </div>
+    </div>
     <div class="container-fluid container-fluid-stop page__content">
       <div class="row">
         <main class="col-12 col-md-8 col-lg-9 main" itemprop="mainContentOfPage" itemscope="itemscope"
