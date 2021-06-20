320e864f75c84c1dc7d270b0c0d72e90d9805179
Michal Staniecko <michal.staniecko@gmail.com>
2021-06-20 10:50:25 +0200

    converterpro fix css

--- wp-content/themes/sardynkibiznesu-2.0/inc/theme-enqueue.php
+++ wp-content/themes/sardynkibiznesu-2.0/inc/theme-enqueue.php
@@ -6,4 +6,5 @@ function sb_theme_enqueue() {
   $css = filemtime(get_stylesheet_directory().'/assets/main.css');
   wp_enqueue_script('index', get_stylesheet_directory_uri().'/assets/index.js', false, $js, true);
   wp_enqueue_style('main', get_stylesheet_directory_uri().'/assets/main.css', false, $css);
+  wp_enqueue_style('cp-popup', plugins_url('convertpro/assets/modules/css').'/cp-popup.min.css', false);
 }
