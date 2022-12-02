82423aeca4d80db1be1536de28151306cb312f89
Michal Staniecko <michal.staniecko@gmail.com>
2022-12-02 21:36:16 +0100

    add parameter rel to sb_button shortcode

--- wp-content/themes/sardynkibiznesu-2.0/inc/shortcodes.php
+++ wp-content/themes/sardynkibiznesu-2.0/inc/shortcodes.php
@@ -5,11 +5,12 @@ add_shortcode('sb_button', 'sb_button');
 function sb_button($atts) {
   $a = shortcode_atts(array(
     'url'   => '#',
-    'label' => __('Sprawdź zawartość kursu', 'sb')
+    'label' => __('Sprawdź zawartość kursu', 'sb'),
+    'rel' => 'nofollow'
   ), $atts);
   ob_start();
   ?>
-  <a href="<?php echo $a['url'] ?>" target="_blank" class="button-outline" rel="nofollow">
+  <a href="<?php echo $a['url'] ?>" target="_blank" class="button-outline" rel="<?php echo $a['rel'] ?>">
     <?php echo $a['label'] ?: 'Sprawdź zawartość kursu'; ?>
   </a>
   <?php
