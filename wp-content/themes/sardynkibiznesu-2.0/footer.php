c66436293f559c89320c9849e562fd58b89fe951
Michal Staniecko <michal.staniecko@gmail.com>
2023-04-29 17:32:02 +0200

    update footer

--- wp-content/themes/sardynkibiznesu-2.0/footer.php
+++ wp-content/themes/sardynkibiznesu-2.0/footer.php
@@ -2,16 +2,52 @@
   <div class="footer__main">
     <div class=" container-fluid container-fluid-stop">
       <div class="row">
-        <div class="col">
+        <div class="col-md-6 col-lg">
           <?php if (is_active_sidebar('footer_about_us')) { ?>
             <?php dynamic_sidebar('footer_about_us'); ?>
           <?php } ?>
         </div>
-        <div class="col">
-
-          <?php if (is_active_sidebar('footer_nav_menu')) { ?>
-            <?php dynamic_sidebar('footer_nav_menu'); ?>
-          <?php } ?>
+        <div class="col-md-6 col-lg-auto footer__menu">
+          <a href="/start/"><strong>Sklep:</strong></a>
+          <ul>
+            <li>
+              <a href="/regulamin-sklepu/">Regulamin sklepu i strony www</a>
+            </li>
+            <li>
+              <a href="/case-study-reklama-lokalnej-firmy-na-facebooku/">Case Study - Reklama na Facebooku</a>
+            </li>
+            <li>
+              <a href="/umowy/">Wzory umów dla małej firmy</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-6 col-lg-auto footer__menu">
+          <strong>Darmowe materiały:</strong>
+          <ul>
+            <li>
+              <a href="/polityka-prywatnosci-dla-strony-bloga-pobierz-wzor/">Darmowa polityka prywatności</a>
+            </li>
+            <li>
+              <a href="/zapis-na-newsletter/">Newsletter</a>
+            </li>
+            <li>
+              <a href="/strefa-sardynek/">Strefa Sardynek</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-6 col-lg-auto footer__menu">
+          <strong>O nas:</strong>
+          <ul>
+            <li>
+              <a href="/kontakt/">Kontakt</a>
+            </li>
+            <li>
+              <a href="/blogu/">O blogu</a>
+            </li>
+            <li>
+              <a href="/tu-zacznij/">Tu zacznij</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
