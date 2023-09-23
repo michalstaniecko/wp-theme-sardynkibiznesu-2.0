2dd5ebc9cff943ee9353809e335b564f22dd5cb3
Michal Staniecko <michal.staniecko@gmail.com>
2023-09-23 22:20:35 +0200

    add accordion styles

---
+++ wp-content/themes/sardynkibiznesu-2.0/blocks/accordion/accordion.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Accordion Block template
+ *
+ * @param array $block The block settings and attributes.
+ * @param string $content The block inner HTML (empty).
+ * @param int $post_id The post ID the block is rendering content against.
+ *                           This is either the post ID currently being displayed inside a query loop,
+ *                           or the post ID of the post hosting this block.
+ * @param array $context The context provided to the block by the post or it's parent block.
+ * @var bool $is_preview True during backend preview render.
+ */
+
+$title = get_field('accordion_title');
+$questions = get_field('accordion_questions');
+
+?>
+
+<div class="faq faq--accordion">
+    <h2 class="h1 ihumbak-anchor faq__title"><?php echo $title ?></h2>
+    <div class="faq__list">
+        <div class="faq__column">
+            <?php foreach ($questions as $index => $item): ?>
+                <div class="faq__item <?php echo $index == 0 ? 'faq__item--active' : null ?>">
+                    <div class="h2 faq__question">
+                        <a
+                                class="faq__link" href="#"
+                                data-target="question-<?php echo $index ?>"
+                        >
+                            <i class="fa fa-caret-right faq__link-icon"></i>
+                            <?php echo $item['question']; ?>
+                        </a>
+                    </div>
+                    <div class="faq__answer" id="question-<?php echo $index ?>">
+                        <div>
+                            <?php echo $item['answer']; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
--- wp-content/themes/sardynkibiznesu-2.0/inc/acf-blocks.php
+++ wp-content/themes/sardynkibiznesu-2.0/inc/acf-blocks.php
@@ -2,39 +2,43 @@
 add_action('acf/init', 'sb_acf_init');
 add_action('init', 'sb_register_acf_blocks');
 
-function sb_acf_init() {
+function sb_acf_init()
+{
 
-  // check function exists
-  if (function_exists('acf_register_block')) {
+    // check function exists
+    if (function_exists('acf_register_block')) {
 
-    // register a featured block
-    acf_register_block(array(
-                         'name'            => 'sb-button',
-                         'title'           => __('Button'),
-                         'description'     => __('Button.'),
-                         'render_callback' => 'sb_acf_block_render_callback',
-                         'category'        => 'formatting',
-                         'icon'            => 'admin-comments',
-                         'keywords'        => array( 'button', 'przycisk' ),
-                       ));
+        // register a featured block
+        acf_register_block(array(
+            'name' => 'sb-button',
+            'title' => __('Button'),
+            'description' => __('Button.'),
+            'render_callback' => 'sb_acf_block_render_callback',
+            'category' => 'formatting',
+            'icon' => 'admin-comments',
+            'keywords' => array('button', 'przycisk'),
+        ));
 
-  }
+    }
 }
 
-function sb_acf_block_render_callback($block, $content, $is_preview) {
+function sb_acf_block_render_callback($block, $content, $is_preview)
+{
 
-  // convert name ("acf/testimonial") into path friendly slug ("testimonial")
-  $slug = str_replace('acf/', '', $block['name']);
+    // convert name ("acf/testimonial") into path friendly slug ("testimonial")
+    $slug = str_replace('acf/', '', $block['name']);
 
-  $category = $block['category'];
+    $category = $block['category'];
 
-  // include a template part from within the "template-parts/block" folder
-  if (file_exists(get_theme_file_path("/template-blocks/{$category}-{$slug}.php"))) {
-    include( get_theme_file_path("/template-blocks/{$category}-{$slug}.php") );
-  }
+    // include a template part from within the "template-parts/block" folder
+    if (file_exists(get_theme_file_path("/template-blocks/{$category}-{$slug}.php"))) {
+        include(get_theme_file_path("/template-blocks/{$category}-{$slug}.php"));
+    }
 }
 
-function sb_register_acf_blocks() {
-  register_block_type(untrailingslashit(get_stylesheet_directory()) . '/blocks/faq');
-  register_block_type(untrailingslashit(get_stylesheet_directory()) . '/blocks/podcast-buttons');
+function sb_register_acf_blocks()
+{
+    register_block_type(untrailingslashit(get_stylesheet_directory()) . '/blocks/faq');
+    register_block_type(untrailingslashit(get_stylesheet_directory()) . '/blocks/podcast-buttons');
+    register_block_type(untrailingslashit(get_stylesheet_directory()) . '/blocks/accordion');
 }
